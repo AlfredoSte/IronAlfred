6d591a58b1c0d8149903a50f1f0b1fa7f94248af
AlfredoSte <87646160+AlfredoSte@users.noreply.github.com>
2021-08-23 11:45:04 +0200

    Create Sakila SQL LAB 11 Vers. 1.sql

---
+++ LAB SQL Day 11/SQL Intro LAB/Sakila SQL LAB 11 Vers. 1.sql
@@ -0,0 +1,29 @@
+-- SQL INTRO LAB Instruction 2
+select * from sakila.actor;
+
+-- SQL INTRO LAB Instruction 3 
+select TITLE from sakila.film;
+
+-- SQL INTRO LAB Instruction 4
+select NAME as Language from sakila.language;
+
+-- SQL INTRO LAB Instruction 5.1
+select store_ID from sakila.store
+
+
+-- SQL INTRO LAB Instruction 5.2
+
+SELECT Count(Distinct Staff_ID)
+FROM sakila.staff;
+
+-- SQL INTRO LAB Instruction 5.3
+SELECT First_name
+From sakila.staff;
+
+
+
+
+
+
+
+
